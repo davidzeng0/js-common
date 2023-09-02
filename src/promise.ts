a9b7c837c7d152639d813e6fb1fd8191f7c5f571
ilikdoge <ilikdoge0@gmail.com>
2023-09-02 15:36:15 -0700

    concurrent promise

--- src/promise.ts
+++ src/promise.ts
@@ -1,9 +1,40 @@
-export const Promises = {
-	resolveAfter(delay: number){
+export class Promises{
+	static resolveAfter(delay: number){
 		return new Promise<void>((resolve) => setTimeout(resolve, delay));
-	},
+	}
 
-	rejectAfter(delay: number){
+	static rejectAfter(delay: number){
 		return new Promise<void>((_, reject) => setTimeout(reject, delay));
 	}
-};
+}
+
+export class ConcurrentPromise<T>{
+	task;
+	abortOnFail;
+
+	promise?: Promise<T>;
+	error?: any;
+
+	constructor(task: () => Promise<T>, abortOnFail = false){
+		this.task = task;
+		this.abortOnFail = abortOnFail;
+	}
+
+	run(){
+		if(this.error)
+			throw this.error;
+		if(!this.promise){
+			this.promise = this.task();
+
+			if(this.abortOnFail)
+				this.promise.catch((e) => this.error = e);
+			this.promise.finally(() => this.promise = undefined);
+		}
+
+		return this.promise;
+	}
+
+	clear(){
+		this.error = undefined;
+	}
+}
